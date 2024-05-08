572ae4ae4bdf4779abdc445480a6d20f54571c12
mskozlova <be-more-lavender@yandex.ru>
2024-05-08 19:45:12 +0100

    add retries & sleeps for tn requests

--- telegram.py
+++ telegram.py
@@ -5,7 +5,8 @@ import requests
 TG_TOKEN = os.getenv("TG_TOKEN")
 
 
-def send_message(chat_id, message):
+def send_message(chat_id, message, is_error=False):
+    # TODO: send errors to a special chat
     url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
     response = requests.get(url)
     return response.status_code
--- tn_parser.py
+++ tn_parser.py
@@ -1,10 +1,15 @@
 import re
 import requests
+from requests.adapters import HTTPAdapter, Retry
+import time
 
 
 TN_INIT_URL = "https://tech-nation-visa.smapply.io/prog/"
 TN_LOGIN_URL = "https://tech-nation-visa.smapply.io/acc/l/"
 TN_APP_URL = "https://tech-nation-visa.smapply.io/prog/app/ds/"
+
+TIMEOUT_S = 2
+SLEEP_BETWEEN_REQUESTS_S = 0.5
 
 
 class Application:
@@ -16,8 +21,11 @@ class Application:
 
 
 def create_session():
+    retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
     session = requests.Session()
-    response = session.get(TN_INIT_URL)
+    session.mount("http://", HTTPAdapter(max_retries=retries))
+    
+    response = session.get(TN_INIT_URL, timeout=TIMEOUT_S)
     response.raise_for_status()
     csrf_token = re.findall("name='csrfmiddlewaretoken' value='([a-zA-Z\d]+)'", response.text)[0]
     return session, csrf_token
@@ -35,7 +43,8 @@ def login(email, password, session, csrf_token):
             "password": password,
             "csrfmiddlewaretoken": csrf_token,
         },
-        allow_redirects=True
+        allow_redirects=True,
+        timeout=TIMEOUT_S,
     )
 
 
@@ -46,7 +55,8 @@ def get_applications(session):
     while True:
         page = session.get(
             url=TN_APP_URL,
-            data={"page": str(page_number)}
+            data={"page": str(page_number)},
+            timeout=TIMEOUT_S
         )
         page.raise_for_status()
         applications.extend(page.json().get("results", []))
@@ -60,7 +70,9 @@ def get_applications(session):
 # TODO: timeouts, sleeps, retries
 def get_last_application(email, password):
     session, csrf_token = create_session()
+    time.sleep(SLEEP_BETWEEN_REQUESTS_S)
     login(email, password, session, csrf_token)
+    time.sleep(SLEEP_BETWEEN_REQUESTS_S)
     applications = get_applications(session)
     
     if len(applications) == 0:
